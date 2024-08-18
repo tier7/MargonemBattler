12fac690b91ea87dcb327c556744ead4f24e0427
tier7 <91539085+tier7@users.noreply.github.com>
2024-08-18 15:56:45 +0200

    Dodanie statystyk mowiacych o pozycji i mozliwosci zrobienia kroku

--- professions/mage.py
+++ professions/mage.py
@@ -4,6 +4,8 @@ class Mage(Character):
     def __init__(self, level):
         super().__init__(level)
         self.update_attributes()
+        self.position = 2
+        self.able_to_walk = False
     def update_attributes(self):
         for level in range(2, self.level + 1):
             if level < 21:
--- professions/paladin.py
+++ professions/paladin.py
@@ -4,6 +4,8 @@ class Paladin(Character):
     def __init__(self, level):
         super().__init__(level)
         self.update_attributes()
+        self.position = 1
+        self.able_to_walk = True
 
     def update_attributes(self):
         for level in range(2, self.level + 1):
